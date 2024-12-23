1853cac91c18b2955592d65c923f8f94dd8999df
rayray39 <raynerlimfy@gmail.com>
2024-12-23 22:50:16 +0800

    Update styling for book cards

--- src/Book.js
+++ src/Book.js
@@ -1,17 +1,18 @@
+
+
 function Book(props) {
-    const listItems = props.books.map(book => <ul key={book.id} className="books-card-display">
-        <li><img src={book.image} alt="image of book" /></li>
-        <li key={'title'} style={{fontSize:'20px', fontWeight:'bold'}}>{book.title}</li>
-        <li key={'authors'}>{`Authors: ${book.authors}`}</li>
-        <li key={'publishedDate'}>{`Published Date: ${book.publishedDate}`}</li>
-        {/* <li key={'description'}>{`Description: ${book.description}`}</li> */}
+    const listItems = props.books.map(book => <div className="books-card-display">
+        <p><img src={book.image} alt="image of book" /></p>
+        <p>{book.title}</p>
+        <p>{`Authors: ${book.authors}`}</p>
+        <p>{`Published Date: ${book.publishedDate}`}</p>
         <details>
             <summary>See description</summary>
             <p>{book.description}</p>
         </details>
 
-        <button className="btn btn-dark" style={{marginTop:"10px", fontFamily: "Georgia, 'Times New Roman', Times, serif"}}>Borrow</button>
-    </ul>)
+        <button id="borrow-button" style={{marginTop:"10px", fontFamily: "Georgia, 'Times New Roman', Times, serif"}}>Borrow</button>
+    </div>)
 
     return <div className="books-page">
         <ul>{listItems}</ul>
