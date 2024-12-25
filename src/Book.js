0834a31d48474784cc9d6cc996c4d45d97913ad4
rayray39 <raynerlimfy@gmail.com>
2024-12-25 15:10:58 +0800

    Update add borrowed book functionality

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,40 @@
+import { useState } from "react";
 import { useUser } from "./UserContext";
+import usersData from './data/users.json';
 
 // display all the fetch and processed books as an unordered list.
 
 function Book(props) {
     const { currentUser } = useUser(); // Access currently logged in user.
+    const [users, setUsers] = useState(usersData);
 
-    const handleBorrow = (bookId) => {
-        console.log(`this book has been borrowed: ${bookId}`);
+    const handleBorrow = (book) => {
+        console.log(`ID of borrowed book: ${book.id}`);
+        console.log(`title of borrowed book: ${book.title}`);
         console.log(`currently logged in user: ${currentUser.username}`)
+        addBorrowedBook(book);
+    }
+
+    const addBorrowedBook = (borrowedBook) => {
+        setUsers((prevUsers) => {
+            return prevUsers.map((user) => {
+                if (user.username === currentUser.username) {
+                    // Add the borrowed_books field if it doesn't exist
+                    const updatedBorrowedBooks = [...(user.borrowed_books || []), borrowedBook];
+                    console.log(`Successfully borrowed: ${borrowedBook.title}`);
+                    return { ...user, borrowed_books: updatedBorrowedBooks };
+                }
+                return user;
+            })
+        })
+    }
+
+    const getBorrowedBooks = () => {
+        // test function to get borrowed books of currently logged in user.
+        const user = users.find((user) => user.username === currentUser.username);
+        user.borrowed_books.forEach(borrowedBook => {
+            console.log(borrowedBook.title);
+        });
     }
 
     const listItems = props.books.map(book => <div className="books-card-display" key={book.id}>
@@ -21,8 +48,9 @@ function Book(props) {
         </details>
 
         <div style={{display:'flex', marginTop:'5px'}}>
-            <button id="borrow-button" onClick={() => handleBorrow(book.id)} style={{marginTop:"10px", fontFamily: "Georgia, 'Times New Roman', Times, serif"}}>Borrow</button>
+            <button id="borrow-button" onClick={() => handleBorrow(book)} style={{marginTop:"10px", fontFamily: "Georgia, 'Times New Roman', Times, serif"}}>Borrow</button>
             <button id="wishlist-button" style={{marginTop:"10px", fontFamily: "Georgia, 'Times New Roman', Times, serif"}}>Add to Wishlist</button>
+            <button onClick={getBorrowedBooks}>get borrowed books</button>
         </div>
     </div>)
 
