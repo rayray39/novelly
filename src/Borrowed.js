e91317c2c023ce78b9214e2832b30386667c7530
rayray39 <raynerlimfy@gmail.com>
2024-12-26 14:12:16 +0800

    Update GET method call in Borrowed.js
    GET method is called when the Borrowed tab in
    the top nav bar is clicked.
    
    Let's transfer the GET method call into Borrowed.js
    where the GET method call is made when the Borrowed
    page mounts.

--- src/Borrowed.js
+++ src/Borrowed.js
@@ -1,9 +1,43 @@
+import { useState, useEffect } from "react"
 import TopNavBar from "./TopNavBar"
+import { useUser } from "./UserContext";
 
 function Borrowed() {
+    const [borrowedBooks, setBorrowedBooks] = useState([]);
+    const {currentUser} = useUser();
+
+    useEffect(() => {
+        const fetchBorrowedBooks = async () => {
+            try {
+                const response = await fetch(`http://localhost:5000/borrowed-books/${currentUser.username}`, {
+                    method: 'GET',
+                })
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                console.log(`Borrowed books: ${data.borrowed_books}`);
+                data.borrowed_books.forEach(book => {
+                    console.log(book.title);
+                });
+                setBorrowedBooks(data.borrowed_books || []);
+            } catch (error) {
+                console.error('Error fetching borrowed books:', error);
+            }
+        }
+
+        if (currentUser) {
+            fetchBorrowedBooks();
+        }
+    }, [currentUser])
+
     return <div className="route-page" id="borrowed-page">
         <TopNavBar />
         <h1>This is the borrowed books page</h1> 
+
+        <div>
+            {borrowedBooks.map((book) => <li key={book.id}>{book.title}</li>)}
+        </div>
     </div>
 }
 
--- src/TopNavBar.js
+++ src/TopNavBar.js
@@ -4,27 +4,6 @@ import { useUser } from "./UserContext";
 function TopNavBar() {
     const {currentUser} = useUser();
 
-    const getBorrowedBooks = async () => {
-        // test function to get borrowed books of currently logged in user.
-        // makes a GET request to server.
-        console.log(`getting borrowed books of: ${currentUser.username}`)
-        try {
-            const response = await fetch(`http://localhost:5000/borrowed-books/${currentUser.username}`, {
-                method: 'GET',
-            })
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            const data = await response.json();
-            console.log(`Borrowed books: ${data.borrowed_books}`);
-            data.borrowed_books.forEach(book => {
-                console.log(book.title);
-            });
-        } catch (error) {
-            console.error('Error fetching borrowed books:', error);
-        }
-    }
-
     return <>
         <nav id="top-nav-bar">
             <ul>
@@ -32,7 +11,7 @@ function TopNavBar() {
             </ul>
             <ul>
                 <li><Link to='/catalogue' className="contrast">catalogue</Link></li>
-                <li><Link to='/borrowed' className="contrast" onClick={getBorrowedBooks}>borrowed</Link></li>
+                <li><Link to='/borrowed' className="contrast">borrowed</Link></li>
                 <li><Link to='/wishlist' className="contrast">wish list</Link></li>
                 <li><Link to='/account' className="contrast">account</Link></li>
             </ul>
