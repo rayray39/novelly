16172dd6ae844bc0f67485c0a3a4ebeaab255059
rayray39 <raynerlimfy@gmail.com>
2024-12-26 13:49:36 +0800

    Update borrowed nav bar to get borrowed books
    Remove the test borrow books button and the function
    to make a GET request to the server for borrowed books
    in Book.js. Shift the function to make the GET request
    into TopNavBar.js, where the function will be called
    when the borrowed nav bar is clicked.

--- src/Book.js
+++ src/Book.js
@@ -31,27 +31,6 @@ function Book(props) {
         }
     }
 
-    const getBorrowedBooks = async () => {
-        // test function to get borrowed books of currently logged in user.
-        // makes a GET request to server.
-        console.log(`getting borrowed books of: ${currentUser.username}`)
-        try {
-            const response = await fetch(`http://localhost:5000/borrowed-books/${currentUser.username}`, {
-                method: 'GET',
-            })
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            const data = await response.json();
-            console.log(`Borrowed books: ${data.borrowed_books}`);
-            data.borrowed_books.forEach(book => {
-                console.log(book.title);
-            });
-        } catch (error) {
-            console.error('Error fetching borrowed books:', error);
-        }
-    }
-
     const listItems = props.books.map(book => <div className="books-card-display" key={book.id}>
         <p><img src={book.image} alt="cover page of book" /></p>
         <p>{book.title}</p>
@@ -65,7 +44,6 @@ function Book(props) {
         <div style={{display:'flex', marginTop:'5px'}}>
             <button id="borrow-button" onClick={() => handleBorrow(book)} style={{marginTop:"10px", fontFamily: "Georgia, 'Times New Roman', Times, serif"}}>Borrow</button>
             <button id="wishlist-button" style={{marginTop:"10px", fontFamily: "Georgia, 'Times New Roman', Times, serif"}}>Add to Wishlist</button>
-            <button onClick={getBorrowedBooks}>get borrowed books</button>
         </div>
     </div>)
 
--- src/TopNavBar.js
+++ src/TopNavBar.js
@@ -1,6 +1,30 @@
 import { Link } from "react-router-dom";
+import { useUser } from "./UserContext";
 
 function TopNavBar() {
+    const {currentUser} = useUser();
+
+    const getBorrowedBooks = async () => {
+        // test function to get borrowed books of currently logged in user.
+        // makes a GET request to server.
+        console.log(`getting borrowed books of: ${currentUser.username}`)
+        try {
+            const response = await fetch(`http://localhost:5000/borrowed-books/${currentUser.username}`, {
+                method: 'GET',
+            })
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log(`Borrowed books: ${data.borrowed_books}`);
+            data.borrowed_books.forEach(book => {
+                console.log(book.title);
+            });
+        } catch (error) {
+            console.error('Error fetching borrowed books:', error);
+        }
+    }
+
     return <>
         <nav id="top-nav-bar">
             <ul>
@@ -8,7 +32,7 @@ function TopNavBar() {
             </ul>
             <ul>
                 <li><Link to='/catalogue' className="contrast">catalogue</Link></li>
-                <li><Link to='/borrowed' className="contrast">borrowed</Link></li>
+                <li><Link to='/borrowed' className="contrast" onClick={getBorrowedBooks}>borrowed</Link></li>
                 <li><Link to='/wishlist' className="contrast">wish list</Link></li>
                 <li><Link to='/account' className="contrast">account</Link></li>
             </ul>
