dfe43e9775726a74d9fc122f34ac1f3d56fefe57
rayray39 <raynerlimfy@gmail.com>
2024-12-27 14:25:23 +0800

    Update formatting of due date

--- src/Borrowed.js
+++ src/Borrowed.js
@@ -9,6 +9,7 @@ function Borrowed() {
     useEffect(() => {
         // the logic will run when the component mounts and everytime the currentUser changes.
         const fetchBorrowedBooks = async () => {
+            // makes a GET request to the server, which returns all the borrowed books in the user's list.
             try {
                 const response = await fetch(`http://localhost:5000/borrowed-books/${currentUser.username}`, {
                     method: 'GET',
@@ -33,6 +34,7 @@ function Borrowed() {
     }, [currentUser])
 
     const handleReturnBook = async (bookId) => {
+        // makes a DELETE request to the server to remove borrowed book from list of borrowed books.
         try {
             const repsonse = await fetch(`http://localhost:5000/return-book/${currentUser.username}/${bookId}`, {
                 method: 'DELETE',
@@ -50,6 +52,15 @@ function Borrowed() {
         }
     }
 
+    function formatDate(dateString) {
+        const date = new Date(dateString);  // convert the string back into a date object.
+        const dd = String(date.getDate()).padStart(2, '0'); // Add leading zero if needed
+        const mm = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+        const yyyy = date.getFullYear();
+
+        return `${dd}-${mm}-${yyyy}`;
+    }
+
     const listItems = borrowedBooks.map((book) => <div className="books-card-display" key={book.id}>
         <p><img src={book.image} alt="cover page of book" /></p>
         <p>{book.title}</p>
@@ -60,7 +71,7 @@ function Borrowed() {
             <p>{book.description}</p>
         </details>
 
-        <p style={{fontWeight:'bold'}}>Due date: {book.dueDate}</p>
+        <p style={{fontWeight:'bold'}}>Due date: {formatDate(book.dueDate)}</p>
 
         <button id="return-button" onClick={() => handleReturnBook(book.id)}>Return</button>
     </div>)
