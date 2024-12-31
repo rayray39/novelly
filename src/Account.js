19e18f00387d1ab072c79dd9b5397b89df0c72a3
rayray39 <raynerlimfy@gmail.com>
2024-12-31 21:10:08 +0800

    Update text inputs to be control components

--- src/Account.js
+++ src/Account.js
@@ -1,18 +1,47 @@
+import { useState } from "react";
 import TopNavBar from "./TopNavBar"
 import { useUser } from "./UserContext";
 
 function Account() {
     const {currentUser} = useUser();
+    const [saveButtonDisabled, setSaveButtonDisabled] = useState(true);
+    const [birthDate, setBirthDate] = useState('');
+    const [email, setEmail] = useState('');
 
     const UserInfoCard = (props) => {
         return <div className="userinfo-card">
             <h3>{props.heading}</h3>
             
             <div style={{display:'flex'}}>
-            <input style={{backgroundColor: 'white'}} type="text" name={props.heading} aria-label="disabled input" placeholder={props.info} disabled/>
-            <button id="account-save-button" disabled>save</button>
+                <input style={{backgroundColor: 'white'}} 
+                    type={props.type} 
+                    name={props.heading}
+                    value={props.value}
+                    onChange={props.onChange}
+                    aria-label="disabled input" 
+                    placeholder={props.info} 
+                    />
+                {props.heading !== "Name" ? <SaveButton />: null}
             </div>
         </div>
+    }
+
+    const SaveButton = () => {
+        console.log(`value of saveButtonDisabled: ${saveButtonDisabled}`);
+        return <>
+            {saveButtonDisabled ?
+                <button id="account-save-button" disabled>save</button> :
+                <button id="account-save-button" >save</button>
+            }
+        </>
+    }
+
+    const getBirthDate = (event) => {
+        setBirthDate(event.target.value)
+    }
+
+    const getEmail = (event) => {
+        setEmail(event.target.value)
     }
 
     return <div className="route-page" id="account-page">
@@ -21,9 +50,9 @@ function Account() {
         <h1 className="main-title-2">ACCOUNT</h1>
         
         <div id="user-info">
-            <UserInfoCard heading="Name" info={currentUser.username}/>
-            <UserInfoCard heading="Date Of Birth" info="this is your birth date"/>
-            <UserInfoCard heading="Email" info="this is your email"/>
+            <UserInfoCard heading="Name" info={currentUser.username} type='text'/>
+            <UserInfoCard heading="Date Of Birth" info={birthDate} type='date' value={birthDate} onChange={getBirthDate}/>
+            <UserInfoCard heading="Email" info={email} type='text' value={email} onChange={getEmail}/>
         </div>
     </div>
 }
