50160526facf21c64fbc241264dae31bb3a58aa4
rayray39 <raynerlimfy@gmail.com>
2025-01-01 17:32:48 +0800

    Update admin page to include cards

--- src/AdminPage.js
+++ src/AdminPage.js
@@ -1,7 +1,26 @@
-function AdminPage() {
-    return <div>
-        <h1>This is the admin page</h1>
+
+function UserCard() {
+    return <div id="admin-card">
+        this is a card
     </div>
+}
+
+function AdminPage() {
+
+    return <>
+        <div className="route-page" id="admin-page">
+            <nav id="top-nav-bar">
+                <ul>
+                    <li><strong>NOVELLY</strong></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <UserCard />
+            <UserCard />
+        </div>
+    </>
 }
 
 export default AdminPage;
